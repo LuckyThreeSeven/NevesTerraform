7d7e2403481c29271990261aa24860258e1dcebf
donggyu <leedong1848@gmail.com>
2025-10-20 10:02:31 +0900

    remove s3 output

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -83,8 +83,3 @@ output "efs_security_group_id" {
   value       = aws_security_group.efs.id
 }
 
-output "s3_bucket_name" {
-  description = "S3 버킷 이름"
-  value       = aws_s3_bucket.main.bucket
-}
-

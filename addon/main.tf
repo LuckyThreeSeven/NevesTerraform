776416df2a6b27c218806a13f320f8a987d74023
uridonget <100407513+uridonget@users.noreply.github.com>
2025-10-22 17:22:12 +0900

    Disable grafana from stack

--- addon/main.tf
+++ addon/main.tf
@@ -256,6 +256,9 @@ resource "helm_release" "prometheus" {
           podMonitorNamespaceSelector: {}
         }
       }
+      grafana = {
+        enabled = false
+      }
     })
   ]
 }
@@ -282,4 +285,31 @@ resource "helm_release" "metrics_server" {
       ]
     })
   ]
+}
+
+# ------------------------------------------------------------------------------
+# Grafana Operator
+# ------------------------------------------------------------------------------
+
+resource "helm_release" "grafana_operator" {
+  name       = "grafana-operator"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "grafana-operator"
+  version    = "v5.9.1"
+  namespace  = "monitoring"
+
+  create_namespace = false
+  timeout          = 600
+  wait             = true
+
+  depends_on = [
+    helm_release.prometheus
+  ]
+
+  # 기본 설정으로 설치 (필요시 values 추가)
+  # values = [
+  #   yamlencode({
+  #     # Operator 관련 설정값 (예: 감시할 네임스페이스 지정 등)
+  #   })
+  # ]
 }
